47d6b55c7ca039331160b2dc256ff7f01ba2b190
root <sebomaduna26@gmail.com>
2024-01-23 13:08:42 -0800

    No 3

---
+++ 3-quick_sort.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+#include <stdio.h>
+
+/**
+  * quick_sort - this function sorts array in ascending order using Quick sort.
+  * @array: pointer to array to be sorted.
+  * @size: the number of elements in the array.
+  */
+void quick_sort(int *array, size_t size)
+{
+	
+}
