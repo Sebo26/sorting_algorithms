6b10ffd0e03ab6ec31a1160f46254f310fe1b2c4
root <sebomaduna26@gmail.com>
2024-01-23 12:22:40 -0800

    No 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -2,25 +2,38 @@
 #include <stdio.h>
 
 /**
-  * selection_sort - the function places integers in ascending order in an array using Selection sort
+  * selection_sort - the function places integers in ascending order in an
+  * array using Selection sort.
   * @array: pointer to array to be sorted.
   * @size: the number of elements in the array.
   */
 void selection_sort(int *array, size_t size)
 {
 	size_t i;
-	int min;
+	size_t m;
+	size_t min;
 	int temp;
+
 	for (i = 0; i < size - 1; i++)
 	{
-		array[0] = min;
-		if (min < array[i])
+		min = i;
+		for (m = i + 1; m < size; m++)
 		{
-			temp = min;
-			min = array[i];
-			array[i] = temp;
+			if (array[m] < array[min])
+			{
+				min = m;
+			}
+		if (min != i)
+		{
+			temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
 		}
-		printf("%d", *array);
-		printf("\n");
-	}	
+		for (m = 0; m < size - 1; m++)
+		{
+			printf("%d, ", array[m]);
+		}
+		printf("%d\n", array[m]);
+		}
+	}
 }
