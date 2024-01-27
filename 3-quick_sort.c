8b50463a4971880f97081f4040eebb1bc88bbfee
root <sebomaduna26@gmail.com>
2024-01-27 13:06:46 -0800

    No 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -10,7 +10,10 @@ void quick_sort(int *array, size_t size)
 {
 	size_t i;
 	int temp;
+	size_t j;
+	int swapped;
 
+	swapped = 0;
 	for (i = 0; i < size - 1; i++)
 	{
 		if (array[i] > array[i + 1])
@@ -23,7 +26,17 @@ void quick_sort(int *array, size_t size)
 				temp = array[size - 1];
 				array[size - 1] = array[size - 2];
 				array[size - 2] = temp;
+				for (j = 0; j < size - 1; j++)
+				{
+					printf("%d, ", array[j]);
+				}
+				printf("%d\n", array[size - 1]);
+				swapped = 2;
 			}
+		}
+		if (!swapped)
+		{
+			break;
 		}
 	}
 }
