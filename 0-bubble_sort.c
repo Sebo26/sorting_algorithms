ed3837a8139f816e67ac0bb4042629ef306ce9db
root <sebomaduna26@gmail.com>
2024-01-27 08:11:57 -0800

    No 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,23 +10,32 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t m;
 	size_t i;
+	size_t j;
 	int temp;
+	int swapped;
 
+	swapped = 0;
 	for (m = 1; m < size; m++)
 	{
-		for (i = 0; i < size - 1; i++)
+		for (i = 0; i < size - m; i++)
 		{
 			if (array[i] > array[i + 1])
 			{
 				temp = array[i];
 				array[i] = array[i + 1];
 				array[i + 1] = temp;
+
+				for (j = 0; j < size - 1; j++)
+				{
+					printf("%d, ", array[j]);
+				}
+				printf("%d\n", array[size - 1]);
+				swapped = 1;
 			}
 		}
-		for (i = 0; i < size - 1; i++)
-		{
-			printf("%d, ", array[i]);
-		}
-		printf("%d\n", array[i]);
+		if (!swapped)
+                {
+                        break;
+                }
 	}
 }
